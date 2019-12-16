3f44c5a3449ac5a3d8d874265900812072e4dc29
@daubim4 <daubim4@gmail.com>
2019-12-16 16:32:36 +0100

    Adding main classes (part 4)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Jewel_piece jewel_piece = new Jewel_piece();
-        System.out.println(jewel_piece.is_destructible());
+        System.out.println(jewel_piece.getIs_destructible());
     }
 }

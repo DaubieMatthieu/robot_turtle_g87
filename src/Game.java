a877c0408455049ed21cbae4f5ea74bb0d6cfb29
@daubim4 <daubim4@gmail.com>
2019-12-16 15:51:24 +0100

    Adding main classes (part 1)

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Game {
+    private ArrayList<Player> player_list;
+    private Board board;
+
+    public void play_game() {}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class Player {
+    private String color;
+    private Turtle_piece piece;
+    private ArrayList<Classic_card> card_deck;
+    private ArrayList<Classic_card> card_hand;
+    private Queue<Classic_card> program;
+    private ArrayList<Wall_card> wall_hand;
+
+    public String get_color() {
+        return this.color;
+    }
+
+
+
+
+}
